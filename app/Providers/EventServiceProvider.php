e30b9173488171714c894ffb9bc77c2910789967
Farid Inawan <frdteknikelektro@gmail.com>
2017-10-19 23:24:57 +0700

    Uncomment listener because not exists yet

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -17,10 +17,10 @@ class EventServiceProvider extends ServiceProvider
             'App\Listeners\EventListener',
         ],
         'Laravel\Passport\Events\AccessTokenCreated' => [
-            'App\Listeners\RevokeOldTokens',
+            // 'App\Listeners\RevokeOldTokens',
         ],
         'Laravel\Passport\Events\RefreshTokenCreated' => [
-            'App\Listeners\PruneOldTokens',
+            // 'App\Listeners\PruneOldTokens',
         ],
     ];
 
