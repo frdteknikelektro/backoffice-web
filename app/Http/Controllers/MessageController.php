a91c26c2ec8c8f95f94e3eb7a6c0ac8b653937bd
Farid Inawan <frdteknikelektro@gmail.com>
2017-10-19 11:07:34 +0700

    Add Message model, controller, migration, and modify relation with User

---
+++ app/Http/Controllers/MessageController.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use App\Message;
+use App\Events\MessageSent;
+use Illuminate\Http\Request;
+
+/**
+ * Message Contoller
+ */
+class MessageController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $messages = Message::with('user')->latest()->paginate();
+
+        if (request()->ajax()) {
+            return response()->json([
+                'code' => 200,
+                'status' => 'success',
+                'data' => $messages
+            ]);
+        }
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'user_id' => 'required|exists:users,id',
+            'message' => 'required|string'
+        ], [], [
+            'user_id' => 'user'
+        ]);
+
+        $user = User::find($request->user_id);
+
+        $message = new Message;
+        foreach ([ 'message' ] as $key) {
+            if ($request->exists($key)) {
+                $message->{$key} = $request->{$key};
+            }
+        }
+        $user->messages()->save($message);
+
+        broadcast(new MessageSent($user, $message))->toOthers();
+
+        if ($request->ajax()) {
+            return response()->json([
+                'code' => 201,
+                'status' => 'success',
+                'data' => $message
+            ]);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Message  $message
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Message $message)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Message  $message
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Message $message)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Message  $message
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Message $message)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Message  $message
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Message $message)
+    {
+        //
+    }
+}
